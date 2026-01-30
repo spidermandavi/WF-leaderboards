9788621730d686af7315613a07f67bc6b310db7f
Rank-8_RK <djcraes28@gmail.com>
2026-01-30 06:16:43 +0100

    Create variants.js

---
+++ scripts/variants.js
@@ -0,0 +1,10 @@
+module.exports = [
+  { variant: "antichess", teamId: "world-antichess-front", perfKey: "antichess" },
+  { variant: "atomic", teamId: "world-atomic-front", perfKey: "atomic" },
+  { variant: "kingOfTheHill", teamId: "world-king-of-the-hill-front", perfKey: "kingOfTheHill" },
+  { variant: "racingKings", teamId: "world-racing-kings-front", perfKey: "racingKings" },
+  { variant: "chess960", teamId: "world-chess-960-front", perfKey: "chess960" },
+  { variant: "threeCheck", teamId: "world-3-check-front", perfKey: "threeCheck" },
+  { variant: "crazyhouse", teamId: "world-crazy-house-front", perfKey: "crazyhouse" },
+  { variant: "worldChessOfficial", teamId: "world-chess-front-official", perfKey: null }
+];
